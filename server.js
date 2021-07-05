8090bf19df12b9d291c7960b49f65b43147b6969
azizamari <azizamari2003@gmail.com>
2021-07-05 13:28:32 +0100

    Server static files and use UUID

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express = require('express');
+const {v4: uuidv4}=require('uuid');
 
 const app = express();
 const server=require('http').Server(app);
 
 app.set('view engine', 'ejs')
 app.set('veiws');
+app.use(express.static('public'));
 
 app.get('/',(req,res)=>{
-    res.render('room');
+    res.redirect(`/${uuidv4()}`);
 });
 
+app.get('/:room',(req,res)=>{
+    res.render('room',{
+        roomId:req.params.room,
+    });
+});
 
 const PORT=process.env.PORT || 3000;
 server.listen(PORT,()=>{
