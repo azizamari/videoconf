360b1ad8fde164e0d75e138e67f66c2d06306118
azizamari <azizamari2003@gmail.com>
2021-07-06 12:03:18 +0100

    Stream video from webcam to browser

--- public/script.js
+++ public/script.js
@@ -1,1 +1,20 @@
-console.log("aziz")
+const videoGrid=document.querySelector('#video-grid');
+const myVideo=document.createElement('video');
+myVideo.muted=true;
+
+let myVideoStream=
+navigator.mediaDevices.getUserMedia({
+    video:true,
+    audio:true
+}).then(stream=>{
+    myvideoStream=stream;
+    addVideoStream(myVideo,stream);
+});
+
+const addVideoStream=(video,stream)=>{
+    video.srcObject=stream;
+    video.addEventListener('loaded+metadata',()=>{
+        video.play();
+    });
+    videoGrid.append(video);
+};
